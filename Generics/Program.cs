088e7f53686874e33824b7ea31841ec8a52dc9a5
Arundhati-Roy <arundhati.roy1998@gmail.com>
2020-10-20 09:05:23 +0530

    Printed Array of Different Types using Generics

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -10,33 +10,18 @@ namespace Generics
             int[] intArray = { 1, 2, 4, 5, 6 };
             double[] doubleArray = { 1.2, 3.4, 5.6 };
             char[] charArray = { 'f', 'h', 'g', 'i' };
-            Program.toPrint(intArray);
-            Program.toPrint(doubleArray);
-            Program.toPrint(charArray);
+            Program.toPrint<int>(intArray);
+            Program.toPrint<double>(doubleArray);
+            Program.toPrint<char>(charArray);
         }
-        public static void toPrint(int[] a)
+        public static void toPrint<T>(T[] a)
         {
-            foreach(int i in a)
+            foreach(var i in a)
             {
                 Console.WriteLine(i);
             }
             Console.WriteLine("\n");
         }
-        public static void toPrint(double[] b)
-        {
-            foreach (int i in b)
-            {
-                Console.WriteLine(i);
-            }
-            Console.WriteLine("\n");
-        }
-        public static void toPrint(char[] c)
-        {
-            foreach (int i in c)
-            {
-                Console.WriteLine(i);
-            }
-            Console.WriteLine("\n");
-        }
+        
     }
 }
