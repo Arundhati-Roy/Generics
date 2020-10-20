2366295fe89d01985eb6100e4500755ede550b25
Arundhati-Roy <arundhati.roy1998@gmail.com>
2020-10-20 09:17:46 +0530

    Refactor. Printed Array of Different Types using Generics Class

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -2,26 +2,36 @@
 
 namespace Generics
 {
+    public class PrintArray<T>
+    {
+        private T[] a;
+        public PrintArray(T[] a)
+        {
+            this.a = a;
+        }
+        public void toPrint()
+        {
+            foreach (var i in a)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("\n");
+        }
+    }
     class Program
     {
+       
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to generics demo!");
             int[] intArray = { 1, 2, 4, 5, 6 };
             double[] doubleArray = { 1.2, 3.4, 5.6 };
             char[] charArray = { 'f', 'h', 'g', 'i' };
-            Program.toPrint<int>(intArray);
-            Program.toPrint<double>(doubleArray);
-            Program.toPrint<char>(charArray);
+            new PrintArray<int>(intArray).toPrint();
+            new PrintArray<double>(doubleArray).toPrint();
+            new PrintArray<char>(charArray).toPrint();
         }
-        public static void toPrint<T>(T[] a)
-        {
-            foreach(var i in a)
-            {
-                Console.WriteLine(i);
-            }
-            Console.WriteLine("\n");
-        }
+       
         
     }
 }
