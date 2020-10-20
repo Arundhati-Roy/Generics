9e8e048cbb534618b23925461c1836105abf2c6b
Arundhati-Roy <arundhati.roy1998@gmail.com>
2020-10-20 09:58:26 +0530

    Maximum of 3 float numbers

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -24,13 +24,14 @@ namespace Generics
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to generics demo!");
-            int[] intArray = { 1, 2, 4, 5, 6 };
+            /*int[] intArray = { 1, 2, 4, 5, 6 };
             double[] doubleArray = { 1.2, 3.4, 5.6 };
             char[] charArray = { 'f', 'h', 'g', 'i' };
             new PrintArray<int>(intArray).toPrint();
             new PrintArray<double>(doubleArray).toPrint();
-            new PrintArray<char>(charArray).toPrint();
+            new PrintArray<char>(charArray).toPrint();*/
             Console.WriteLine(MaxOf3(3, 4, 5));
+            Console.WriteLine(MaxOf3(3.4, 6.7, 1.2));
         }
         public static int MaxOf3(int a,int b,int c)
         {
@@ -42,7 +43,18 @@ namespace Generics
                 return c;
             return a;
         }
-       
-        
+        public static double MaxOf3(double a, double b, double c)
+        {
+            if ((a > b && a > c) || (a >= b && a > c) || (a > b && a >= c))
+                return a;
+            if ((b > a && b > c) || (b >= c && b > c) || (b > a && b >= c))
+                return b;
+            if ((c > b && c > a) || (c >= b && c > a) || (c > b && c >= a))
+                return c;
+            return a;
+        }
+
+
+
     }
 }
